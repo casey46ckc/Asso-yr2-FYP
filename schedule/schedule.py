7bb327ba135663a96a13fb223d565bc4926bcbc2
patrickpschow <50395399+patrickpschow@users.noreply.github.com>
2021-02-20 09:28:39 +0800

    Update: recursion function complete, wait for integration

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -1,6 +1,8 @@
 import pandas
 import numpy
+from time import process_time 
 
+# Object: represent the fundanmental class schedule information for add/drop/swap
 class TheClassSchedule:
     def __init__(self, clCode, clNo, clName, sem, clRoom, weekday, timeSlot):
         self.clCode = clCode
@@ -20,6 +22,7 @@ class TheClassSchedule:
         # print(f"{self.clCode}-{self.clNo} after addition:\n", self.timeTable)
 
 
+# functionality: check whether the input courses are feasible without location and time conflict, return None if it is not feasible, return combined time table in 7 x 6 matrix format
 def combinationCheck(*args):
     timeTable = numpy.zeros((7, 6), dtype=int)
     for i in range(len(args)):
@@ -43,6 +46,15 @@ def combinationCheck(*args):
         return timeTable
 
 
+# functionality: combined the time table of TheClassSchedule Object in 7 x 6 matrix format without checking
+def combinedTable(*args):
+    timeTable = numpy.zeros((7, 6), dtype=int)
+    for i in range(len(args)):
+        timeTable += args[i].timeTable
+    return timeTable
+
+
+# functionality: combined the time table in 7 x 6 matrix format without checking
 def readClSchedule(fileName):
     clList = []
     data = pandas.read_excel(fileName, dtype={'Course Code': str, 'Class No': str, 'Course Name': str, 'Semester': int, 'Weekday': int, 'Time': str, 'Room': str})
@@ -64,6 +76,7 @@ def readClSchedule(fileName):
     return clList
 
 
+# functionality: check the number of dayoff for the time table in 7 x 6 matrix format, return in integer primitive type
 def countDayoff(timeTable):
     dayOffCounter = 0
     for i in range(6):
@@ -72,11 +85,94 @@ def countDayoff(timeTable):
     return dayOffCounter
 
 
+# functionality: check the existence of early lesson (start at 8:30 a.m.) for the time table in 7 x 6 matrix format, return True when exists
 def earlyLesson(timeTable):
     if numpy.max(timeTable[0,:]) == 0:
         return True
     else:
         return False
+
+
+# functionality: recursive function for product of N class in object approach
+# args[0]:start depth, args[1]:current depth, arg[0]...arg[n]:clsList, replaced by clsList[0][i], clsList[1][j], ... one by one recursively
+# original version for debugging, version 1 for performance
+def collect_result(*args):
+    if args[1] < args[0]:
+        for i in range(len(args[args[1] + 2])):
+            paras_next = []
+            paras_next.append(args[0])
+            paras_next.append(args[1] + 1)
+            for k in range(args[0]):
+                print(f"i: {i}\tk: {k}")
+                if k != args[1]:
+                    if isinstance(args[k + 2], TheClassSchedule):
+                        print(f"i: {i}\tk: {k}\tCl Code:{args[k + 2].clCode}{args[k + 2].clNo}")
+                    paras_next.append(args[k + 2])
+                else:
+                    paras_next.append(args[k + 2][i])
+                    print(f"i: {i}\tk: {k}\tCl Code:{args[k + 2][i].clCode}{args[k + 2][i].clNo}")
+    
+            for l in range(len(paras_next)):
+                if isinstance(paras_next[l], list):
+                    for m in range(len(paras_next[l])):
+                        if isinstance(paras_next[l][m], TheClassSchedule):
+                            print(f"paras_next[{l}][{m}]: {paras_next[l][m].clCode}{paras_next[l][m].clNo}")
+                elif isinstance(paras_next[l], TheClassSchedule):
+                    print(f"paras_next[{l}]: {paras_next[l].clCode}{paras_next[l].clNo}")
+                else:
+                    print(f"paras_next[{l}]: {paras_next[l]}")
+            collect_result(*paras_next)
+
+    else:
+        tmpTable = combinationCheck(*args[2:])
+        if(tmpTable is not None):
+            clCodeList = [cl.clCode + cl.clNo for cl in args[2:]]
+            print(f"{'+'.join(clCodeList)}|Early Lesson: {earlyLesson(tmpTable)} No. of day-off: {countDayoff(tmpTable)}\n{tmpTable}")
+
+
+
+def collect_result_V1(depth, curr_depth, *args):
+    if curr_depth < depth:
+        for i in range(len(args[curr_depth])):
+            paras_next = []
+            for k in range(depth):
+                if k != curr_depth:
+                    paras_next.append(args[k])
+                else:
+                    paras_next.append(args[k][i])
+            collect_result_V1(depth, curr_depth + 1, *paras_next)
+    else:
+        tmpTable = combinationCheck(*args)
+        if(tmpTable is not None):
+            clCodeList = [cl.clCode + cl.clNo for cl in args]
+            print(f"{'+'.join(clCodeList)}|Early Lesson: {earlyLesson(tmpTable)} No. of day-off: {countDayoff(tmpTable)}\n{tmpTable}")
+    
+
+def collect_result_V2(depth, curr_depth, *args):
+    if curr_depth < depth:
+        for i in range(len(args[curr_depth])):
+            paras_next = []
+            for k in range(depth):
+                if k != curr_depth:
+                    paras_next.append(args[k])
+                else:
+                    paras_next.append(args[k][i])
+
+            if curr_depth + 1 > 1:
+                tmpTable = combinationCheck(*paras_next[:curr_depth + 1])
+                if tmpTable is not None:
+                    collect_result_V2(depth, curr_depth + 1, *paras_next)
+            else:
+                collect_result_V2(depth, curr_depth + 1, *paras_next)
+    else:
+        tmpTable = combinationCheck(*args)
+        if(tmpTable is not None):
+            clCodeList = [cl.clCode + cl.clNo for cl in args]
+            print(f"{'+'.join(clCodeList)}|Early Lesson: {earlyLesson(tmpTable)} No. of day-off: {countDayoff(tmpTable)}\n{tmpTable}")
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -85,7 +181,7 @@ if __name__ == "__main__":
     tmpTable = None
 
     clS = ['CCCH4003CL54', 'CCCU4041', 'CCEN4005', 'CCIT4033CL03', 'CCIT4059CL03', 'CCIT4080']
-    clK = ['CCIT4033CL03', 'CCIT4059CL03']
+    clK = ['CCIT4033', 'CCIT4080']
     clSList = []
     clKList = []
     loopDepth = len(clS)
@@ -102,35 +198,14 @@ if __name__ == "__main__":
         else:
             clKList.append([cl for cl in clList if cl.clCode == clK[i]])
 
-    # args[0]:start depth, args[1]:current depth, arg[2]...arg[n + 2]:clsList, replaced by clsList[0][i], clsList[1][j], ... one by one recursively
-    def collect_result(*args):
-        if args[1] != 0:
-            for i in range(len(args[args[1]]) + 1):
-                paras_next = []
-                paras_next.append(args[0])
-                paras_next.append(args[1] - 1)
-                for k in range(args[0]):
-                    print(f"start depth:{args[0]} current depth:{args[1]} i: {i} next depth: {args[1] - 1} k: {k} Code:{args[k + 2][i].clCode}")
-                    if k == args[1]:
-                        paras_next.append(args[k + 2][i])
-                    else:
-                        paras_next.append(args[k + 2])
-                collect_result(paras_next)
-        else:
-            tmpTable = combinationCheck(args[2:])
-            if(tmpTable is not None):
-                clCodeList = [cl.clCode + cl.clNo for cl in args[2:]]
-                print(f"{'+'.join(clCodeList)}|Early Lesson: {earlyLesson(tmpTable)} No. of day-off: {countDayoff(tmpTable)}\n{tmpTable}")
+    t1_start = process_time()
+    collect_result_V1(loopDepth, 0, *clSList)
+    t1_stop = process_time()
+    print("Elapsed time during the V1 in seconds:", t1_stop - t1_start)  
 
-    for i in range(len(clSList[0])):
-        for j in range(len(clSList[1])):
-            for k in range(len(clSList[2])):
-                for l in range(len(clSList[3])):
-                    for m in range(len(clSList[4])):
-                        for n in range(len(clSList[5])):
-                            tmpTable = combinationCheck(clSList[0][i], clSList[1][j], clSList[2][k], clSList[3][l], clSList[4][m], clSList[5][n])
-                            if(tmpTable is not None):
-                                print(f'{clSList[0][i].clCode}{clSList[0][i].clNo}+{clSList[1][j].clCode}{clSList[1][j].clNo}+{clSList[2][k].clCode}{clSList[2][k].clNo}+{clSList[3][l].clCode}{clSList[3][l].clNo}+{clSList[4][m].clCode}{clSList[4][m].clNo}+{clSList[5][n].clCode}{clSList[5][n].clNo}|Early Lesson: {earlyLesson(tmpTable)} No. of day-off: {countDayoff(tmpTable)}\n{tmpTable}')
-
-    # collect_result(loopDepthK, loopDepthK, clKList)
+    t1_start = process_time()
+    collect_result_V2(loopDepth, 0, *clSList)
+    t1_stop = process_time()
+    print("Elapsed time during the V2 in seconds:", t1_stop - t1_start)  
+    
 
