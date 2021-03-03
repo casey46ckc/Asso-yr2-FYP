ea579a13b4516b19bd940a528dc3b2fd30b1da9b
Patrick <patrickchow@gmail.com>
2021-03-03 10:43:55 +0800

    Update readjson.py

--- readjson.py
+++ readjson.py
@@ -43,3 +43,41 @@ def replace_AbbrName(inStr):
             if inStr.find(obj[i]['term'][j]) != -1:
                 resultTxt = resultTxt.replace(obj[i]['term'][j], obj[i]['abbr'])                
     return resultTxt
+
+def display_json(obj):
+    print(type(obj))
+    if isinstance(obj, dict):
+        for key in obj:
+            print(f'[{key}]:')
+            display_json(obj[key])
+    elif isinstance(obj, list):
+        for i in range(len(obj)):
+            print(f'[{i}]:')
+            display_json(obj[i])
+    elif isinstance(obj, str):
+        print(f': {obj}')
+
+
+if __name__ == "__main__":
+    inFile = open('json/campus_have.json', 'r')
+    fileRead = inFile.read()
+    jsonIn = json.loads(fileRead)
+    print(f'len: {len(jsonIn)}')
+    for key in jsonIn:
+        if isinstance(jsonIn[key], dict):
+            for secondKey in jsonIn[key]:
+                if isinstance(jsonIn[key][secondKey], list):
+                    for obj_index in range(len(jsonIn[key][secondKey])):
+                        print(f'[{key}][{secondKey}][{obj_index}]: {type(jsonIn[key][secondKey][obj_index])} {jsonIn[key][secondKey][obj_index]}')
+                elif isinstance(jsonIn[key][secondKey], dict):
+                    for thirdkey in jsonIn[key][secondKey]:
+                        print(f'[{key}][{secondKey}][{thirdkey}]: {type(jsonIn[key][secondKey][thirdkey])} {jsonIn[key][secondKey][thirdkey]}')
+                elif isinstance(jsonIn[key][secondKey], str):
+                    print(f'[{key}][{secondKey}]: {type(jsonIn[key][secondKey])} {jsonIn[key][secondKey]}')
+        elif isinstance(jsonIn[key], list):
+            for obj_index in range(len(jsonIn[key])):
+                print(f'[{key}][{obj_index}]: {type(jsonIn[key][obj_index])} {jsonIn[key][obj_index]}')
+        elif isinstance(jsonIn[key], str):
+            print(f'[{key}]: {type(jsonIn[key])} {jsonIn[key]}')
+
+    display_json(jsonIn)
